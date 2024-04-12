94decfed5db54ed1dde7a9fe333f72295470f952
Evan Kennedy <evanckennedy@protonmail.com>
2024-04-12 13:28:03 -0500

    fetch users from api

--- assets/script/home.js
+++ assets/script/home.js
@@ -12,3 +12,32 @@ const profilePictures = utils.selectAll('.person-container img');
 const fullNames = utils.selectAll('.person-container p:nth-of-type(1)');
 const cities = utils.selectAll('.person-container p:nth-of-type(2)');
 
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  getUsers                                            */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+const options = {
+  method: 'GET',
+  headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+  mode: 'cors'
+}
+
+const URL = 'https://randomuser.me/api/?nat=CA&results=10&seed=same';
+
+async function getUsers(endpoint) {
+  try {
+    const result = await fetch(endpoint, options);
+
+    if (!result.ok) {
+      // number, then name of the error
+      throw new Error(`${result.statusText} (${result.status})`)
+    }
+
+    const data = await result.json();
+    console.log(data.results);
+  } catch(error) {
+    console.log(error.message);
+  }
+}
+
+getUsers(URL);
+
