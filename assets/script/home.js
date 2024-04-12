b06a46c72fa781a9ec0d5088887afcc510b86c84
Evan Kennedy <evanckennedy@protonmail.com>
2024-04-12 13:25:21 -0500

    link home.js to home.html

--- assets/script/home.js
+++ assets/script/home.js
@@ -6,7 +6,9 @@ import * as utils from './utils.js';
 
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-/*  Organizer                                            */
+/*  Selectors                                            */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+const profilePictures = utils.selectAll('.person-container img');
+const fullNames = utils.selectAll('.person-container p:nth-of-type(1)');
+const cities = utils.selectAll('.person-container p:nth-of-type(2)');
 
-utils.print('Welcome');
